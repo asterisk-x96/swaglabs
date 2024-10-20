74fda484c53dcc45ba4c094e6f2d9d6a57b8e82e
Thao Pham <thachthao.6996@gmail.com>
2024-10-20 20:54:06 +0100

    Finish login test cases

--- tests/e2e/login.spec.ts
+++ tests/e2e/login.spec.ts
@@ -4,30 +4,53 @@ import { LoginPage } from '../page-objects/login';
 const chromium = require('playwright').chromium;
 
 test.describe('Login', () => {
-      //Launch the browser
+  let page;
+  let browser;
+  let context;
+  let loginPage: LoginPage;
 
-  test('login with correct username and password', async () => {
-    const browser = await chromium.launch({ headless: false });
-    const context = await browser.newContext();
-    const page = await context.newPage();
-    
+  test.beforeEach(async () => {
+    browser = await chromium.launch({ headless: false });
+    context = await browser.newContext();
+    page = await context.newPage();
     // Navigate to the login page
-    const loginPage = new LoginPage(page);
+    loginPage = new LoginPage(page);
     // Go to the URL
     await page.goto('https://www.saucedemo.com');
-  
-    const username = loginPage.usernames[Math.floor(Math.random() * loginPage.usernames.length)];
-  
-    await page.fill('#user-name', username);
-    await page.fill('#password', loginPage.password);
-    await page.click('#login-button');
-  
-    const inventoryVisible = await page.isVisible('.inventory_list');
-    expect(inventoryVisible).toBeTruthy();
+  })
+
+  test.afterEach(async () => { 
     await browser.close();
   });
 
+  test('login with correct username and password', async () => {
+    const username = loginPage.usernames[Math.floor(Math.random() * loginPage.usernames.length)];
+    console.log(username);
+    await loginPage.login(username, loginPage.password);
+    const inventoryVisible = await page.isVisible('.inventory_list');
+    expect(inventoryVisible).toBeTruthy();
+  });
 
+  test('login with incorrect username', async () => {
+    const username = "thao";
+    await loginPage.login(username, loginPage.password);
+    const inventoryVisible = await page.isVisible('.inventory_list');
+    expect(inventoryVisible).toBeFalsy();
+  });
+
+  test('login with incorrect password', async () => {
+    const username = loginPage.usernames[Math.floor(Math.random() * loginPage.usernames.length)];
+    await loginPage.login(username, "thao");
+    const inventoryVisible = await page.isVisible('.inventory_list');
+    expect(inventoryVisible).toBeFalsy();
+  });
+
+  test('login with incorrect username and password', async () => {
+    const username = "standard_user_123";
+    await loginPage.login(username, "secret_sauce_123");
+    const inventoryVisible = await page.isVisible('.inventory_list');
+    expect(inventoryVisible).toBeFalsy();
+  });
   
 })
 
--- tests/page-objects/login.ts
+++ tests/page-objects/login.ts
@@ -14,4 +14,10 @@ export class LoginPage {
     constructor(page: Page) {
         this.page = page;
     }
+
+    async login(username: string, password: string) {
+        await this.page.fill('#user-name', username);
+        await this.page.fill('#password', password);
+        await this.page.click('#login-button');
+    }
 }
